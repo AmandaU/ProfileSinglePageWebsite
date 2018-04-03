77790c2a810da724da497eed104cac0f7bd0a422
Amanda Baret <amandau@cowboyaliens.com>
2018-04-03 09:28:33 +0200

    added social media

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,45 +3,109 @@
     <div class="navcolumn">
       
         <h1>{{ slogan }}</h1>
+        <h2>{{ designername }}</h2>
             
+    </div>
+
+  <div class="projectcontainer">
+       <div class="socialmedia">
+
+         <br>
+
+          <a target="_blank" title="follow me on Twitter" href="http://www.twitter.com/ursling">
+          <img alt="follow me on twitter" src="https://c866088.ssl.cf3.rackcdn.com/assets/twitter40x40.png" border=0>
+          </a>
+        <br>
+
+          <a target="_blank" title="follow me on youtube" href="http://www.youtube.com/PLACEHOLDER">
+          <img alt="follow me on youtube" src="https://c866088.ssl.cf3.rackcdn.com/assets/youtube40x40.png" border=0>
+          </a>
+         <br>
+
+          <a target="_blank" title="follow me on instagram" href="http://www.instagram.com/urslingz">
+          <img alt="follow me on instagram" src="https://c866088.ssl.cf3.rackcdn.com/assets/instagram40x40.png" border=0>
+          </a>
+        <br> 
+          <a target="_blank" title="follow us on instagram" href="http://www.instagram.com/urslingz">
+          <img alt="follow us on instagram" src="https://c866088.ssl.cf3.rackcdn.com/assets/badgeinstagram.png" border=0>
+          </a>
+        <br> 
+          <a href="https://www.instagram.com/urslingz/?ref=badge" class="openig-b- openig-b-16">
+          <img src="//badges.instagram.com/static/images/ig-badge-16.png" alt="Instagram" /> 
+          <b>Follow on Instagram</b>
+          </a>
+        <br>  
+          <a href="https://www.instagram.com/urslingz/?ref=badge" class="ig-b- ig-b-v-24">
+          <img src="//badges.instagram.com/static/images/ig-badge-view-24.png" alt="Instagram" />
+          </a>
+        <br> 
+
+            <a target="_blank" title="follow me on facebook" href="https://www.facebook.com/ursling">
+            <img alt="follow me on facebook" src="https://c866088.ssl.cf3.rackcdn.com/assets/facebook40x40.png" border=0></a>
+        <br>     
+
+          <!--vue-goodshare></vue-goodshare>
+        <br> 
+            <vue-goodshare-facebook 
+              button_design="gradient"
+              has_square_edges
+              page_url="https://www.facebook.com/ursling" 
+              title_social="Facebook"
+              has_counter
+              has_icon >
+            </vue-goodshare-facebook>
+        <br> 
+            <vue-goodshare-twitter 
+              button_design="gradient"
+              page_url="https://vuejsfeed.com/" 
+              has_icon 
+              has_square_edges>
+          </vue-goodshare-twitter-->
       </div>
-
- <div class="projectcontainer">
-
-    <vue-goodshare-facebook 
-    page_url="https://github.com" 
-    title_social="Facebook"
-    has_counter
-    has_icon 
-  ></vue-goodshare-facebook>
-
-    <vue-goodshare-twitter 
-   button_design="gradient"
-   page_url="https://vuejsfeed.com/" 
-   has_icon 
-   has_square_edges
-  ></vue-goodshare-twitter>
-    
   </div>
  </div>
 </template>
 
 <script>
+// import VueGoodshare from 'vue-goodshare'
+// import VueGoodshareFacebook from 'vue-goodshare/src/providers/Facebook.vue'
+// import VueGoodshareTwitter from 'vue-goodshare/src/providers/Twitter.vue'
+//import VueInstagram from 'vue-instagram'
 
 export default {
+ name: 'Jade',
 
-  name: 'Jade',
-   data () {
-    return {
-      slogan: 'Contact me',
-      designername: 'I am awesome' 
-    }
-  }
+// components: {
+//    VueInstagram,
+//    VueGoodshare,
+//    VueGoodshareTwitter,
+//    VueGoodshareFacebook
+//   } ,
   
+  methods: 
+ { 
+   fetchToken ()
+   {
+     this.accessToken = '';
+   }
+ },
+   data : function ()  {
+    return {
+       slogan: 'Contact me',
+      designername: 'I am awesome' ,
+      accessToken : ''
+      }
+  },
+
+created: function () {
+  this.fetchToken();
+  	},
+ 
 }
 </script>
 
 <style lang="scss" scoped>
   @import "~@/styles/styles.scss";
+   @import "~@/styles/contactstyles.scss";
    @import "~@/styles/leftbarstyles.scss";
  </style>
--- src/main.js
+++ src/main.js
@@ -7,21 +7,20 @@ import VueFire from 'vuefire'
 
 Vue.use(VueFire)
 
-import VueGoodshare from 'vue-goodshare'
+// import VueGoodshare from 'vue-goodshare'
 
-// Import vue-goodshare single elements
-import VueGoodshareFacebook from 'vue-goodshare/src/providers/Facebook.vue'
-import VueGoodshareTwitter from 'vue-goodshare/src/providers/Twitter.vue'
-
+// // Import vue-goodshare single elements
+// import VueGoodshareFacebook from 'vue-goodshare/src/providers/Facebook.vue'
+// import VueGoodshareTwitter from 'vue-goodshare/src/providers/Twitter.vue'
 
  new Vue({
    el: '#app',
   
   router,
    template: '<App/>',
-  components: { App ,
-    VueGoodshareTwitter,
-    VueGoodshareFacebook
+  components: { App 
+    // ,   VueGoodshareTwitter,
+    // VueGoodshareFacebook
   } 
  
  })
