5fcf211b0ecf2c025873c3aa2e8adffb92081227
Amanda Baret <amandau@cowboyaliens.com>
2018-04-27 13:38:04 +0200

    menu

--- src/App.vue
+++ src/App.vue
@@ -10,41 +10,52 @@
     <div class="floatingmenu">
       <div class="nav hnav">
          <ul>
-              <li><div class="allitem">
-                  <div class="hoveritem" v-on:click="toggle()">+ | </div>
-                  <div class="hoveritem" v-on:click="showAll()">  Projects</div>
-               </div>
+              <li>
+                  <div class="hoveritem" v-on:click="toggle()">Projects </div>
+                   <!-- <div class="hoveritem" v-on:click="toggle()">  +  </div> -->
               </li>
+              <li>
+             <div class="hitem" v-show="isSubMenuOpen" v-on:click="filterProjects('all')">All</div>
+            <li>
+              <div class="hitem" v-show="isSubMenuOpen" v-on:click="filterProjects('photography')">Photography</div>
+            </li>
+            <li> <div class="hitem" v-show="isSubMenuOpen" v-on:click="filterProjects('illustration')">Illustration</div>
+            </li>
+            <li> <div class="hitem" v-show="isSubMenuOpen" v-on:click="filterProjects('styling')">Styling</div>
+            </li>
             <li v-on:click="close()" ><router-link  to="/jade">About</router-link></li>
             <li v-on:click="close()"  ><router-link  to="/contact">Contact</router-link></li>
           </ul>
       </div>
-      <div class="nav vnav" v-show="isSubMenuOpen">
-            <ul ><li><div class="hitem" v-on:click="filterProjects('photography')">Photography</div>
-            </li><li> <div class="hitem" v-on:click="filterProjects('illustration')">Illustration</div>
-            </li><li> <div class="hitem" v-on:click="filterProjects('styling')">Styling</div>
-            </li></ul>
-      </div>
+      <!-- <div class="nav vnav" v-visible="isSubMenuOpen">
+         <ul >
+           <li>
+             <div class="hitem" v-on:click="filterProjects('all')">All</div>
+            <li>
+              <div class="hitem" v-on:click="filterProjects('photography')">Photography</div>
+            </li>
+            <li> <div class="hitem" v-on:click="filterProjects('illustration')">Illustration</div>
+            </li>
+            <li> <div class="hitem" v-on:click="filterProjects('styling')">Styling</div>
+            </li>
+         </ul>
+      </div> -->
     </div>
-      
-  </div>
+   </div>
   
 </template>
 
-
 <script>
+
 export default {
   name: 'app',
  
   data () {
     return {
-  
-    isSubMenuOpen: false,
-    
-    }},
+      isSubMenuOpen: false,
+        }},
 
   methods: {
-
      filterProjects (filter) {
      this.$router.push(
         {
@@ -53,11 +64,9 @@ export default {
      )
  },
     open() {
-     
       this.isSubMenuOpen = true;
     },
     close() {
-     
       this.isSubMenuOpen = false;
     },
     toggle() {
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@
  import App from './App'
 import router from './router'
 import VueFire from 'vuefire'
+import VueVisible from 'vue-visible';
+
+Vue.use(VueVisible);
 
 Vue.use(VueFire)
 
